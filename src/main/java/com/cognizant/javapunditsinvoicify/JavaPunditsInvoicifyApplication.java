6ae182cdd04f091314028bbb54e37a773e426ec0
Mohammad Javed <ghazanfar9131@gmail.com>
2021-05-06 19:43:24 -0400

    - Initialize the First Project.

---
+++ src/main/java/com/cognizant/javapunditsinvoicify/JavaPunditsInvoicifyApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.javapunditsinvoicify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPunditsInvoicifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPunditsInvoicifyApplication.class, args);
+    }
+
+}
