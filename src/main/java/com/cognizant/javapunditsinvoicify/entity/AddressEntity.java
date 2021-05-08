8504dcc87c72b71b7694713956eedf068d1343f1
KB <kbhar01.us@gmail.com>
2021-05-08 11:42:14 -0500

    Refactor: Add company with Address entity completed. RestDocs need to be added.

---
+++ src/main/java/com/cognizant/javapunditsinvoicify/entity/AddressEntity.java
@@ -0,0 +1,27 @@
+package com.cognizant.javapunditsinvoicify.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name="address")
+public class AddressEntity {
+    @Id
+    @JsonIgnore
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String line1;
+    String line2;
+    String city;
+    String state;
+    Integer zip;
+}
