404c028718ef7e61e0738ac57e57df91a6f26ed7
joseantoniovegaruiz <joseantonio.vegaruiz@gmail.com>
2021-05-08 10:51:54 -0500

    RED TEST ISSUE#8

---
+++ src/test/java/com/cognizant/javapunditsinvoicify/integration/CompanyIT.java
@@ -0,0 +1,41 @@
+package com.cognizant.javapunditsinvoicify.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@ActiveProfiles("qa")
+public class CompanyIT {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DirtiesContext()
+    public void updateCompany_Success(){
+
+        RequestBuilder updaterequest=put("/company/1")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+
+    }
+
+
+
+}
