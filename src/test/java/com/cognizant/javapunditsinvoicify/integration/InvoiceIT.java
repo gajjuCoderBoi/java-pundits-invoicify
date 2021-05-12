38cb425981ec91d9b39d9e42e0714a575e814c66
Mohammad Javed <ghazanfar9131@gmail.com>
2021-05-12 15:39:21 -0400

    Issues #5: Added Rest Docs.

--- src/test/java/com/cognizant/javapunditsinvoicify/integration/InvoiceIT.java
+++ src/test/java/com/cognizant/javapunditsinvoicify/integration/InvoiceIT.java
@@ -16,12 +16,15 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.annotation.DirtiesContext.MethodMode.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -48,8 +51,7 @@ public class InvoiceIT {
                 .param("invoice_id", "1")
                 .accept(APPLICATION_JSON)
                 .contentType(APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(sampleInvoiceItemDto))
-                ;
+                .content(objectMapper.writeValueAsString(sampleInvoiceItemDto));
 
         mockMvc.perform(postInvoiceItem)
                 .andExpect(status().isNotFound())
@@ -70,18 +72,32 @@ public class InvoiceIT {
                 .param("invoice_id", invoiceId)
                 .accept(APPLICATION_JSON)
                 .contentType(APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(sampleInvoiceItemDto))
-                ;
+                .content(objectMapper.writeValueAsString(sampleInvoiceItemDto));
 
         mockMvc.perform(postInvoiceItem)
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("responseMessage").value("Invoice Item Successfully Added."))
                 .andDo(print())
+                .andDo(document("invoice-add-item",
+                        requestParameters(
+                                parameterWithName("invoice_id").description("Invoice Id to add Item into.")
+                        ),
+                        requestFields(
+                                fieldWithPath("description").description("Item Description.").type("String"),
+                                fieldWithPath("feeType").description("Type of the Item. ").type("String: FLAT, RATE"),
+                                fieldWithPath("quantity").description("Quantity of An Item. (Only Populate when feeType:rate)").type("Integer"),
+                                fieldWithPath("rate").description("Rate of the Item. (Only Populate when feeType:rate)").type("Double"),
+                                fieldWithPath("amount").description("Item Amount.  (Only Populate when feeType:rate) (Only Populate when feeType:flat)").type("Double")
+                                ),
+                        responseFields(
+                                fieldWithPath("responseMessage").description("Response Message i.e Success Message or Error Message. ")
+                        )
+                ));
         ;
     }
 
     private String postEmptyInvoice() throws Exception {
         return mockMvc.perform(post("/invoice"))
-        .andReturn().getResponse().getContentAsString();
+                .andReturn().getResponse().getContentAsString();
     }
 }
