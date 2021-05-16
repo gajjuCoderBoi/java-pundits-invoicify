b693fd1e22c7eef0b52ef9e7b34205cd8aacd589
Mohammad Javed <ghazanfar9131@gmail.com>
2021-05-15 23:21:56 -0400

    Fixed Rest Docs + Tests.

--- src/main/java/com/cognizant/javapunditsinvoicify/mapper/CompanyMapper.java
+++ src/main/java/com/cognizant/javapunditsinvoicify/mapper/CompanyMapper.java
@@ -2,13 +2,13 @@ package com.cognizant.javapunditsinvoicify.mapper;
 
 import com.cognizant.javapunditsinvoicify.dto.CompanyDto;
 import com.cognizant.javapunditsinvoicify.entity.CompanyEntity;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
+import org.mapstruct.*;
 
-import static org.mapstruct.ReportingPolicy.IGNORE;
-
-@Mapper(unmappedTargetPolicy = IGNORE, unmappedSourcePolicy = IGNORE)
+@Mapper(
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
 public interface CompanyMapper {
     @Mappings({
             @Mapping(source = "dto.name", target = "name"),
